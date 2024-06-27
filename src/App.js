6354bc302dae2d3928533bf1ef3919888f803d23
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-06-27 22:59:02 +0700

    udah

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Login from './pages/login';
+import HubungiKami from './pages/hubungi-kami';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Home/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
+        <Route path="/hubungi-kami" element={<HubungiKami/>}></Route>
       </Routes>
     </Router>
   );
---
+++ src/pages/hubungi-kami.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavbarHome from '../components/navbar-home';
+import Footer from '../components/footer';
+function HubungiKami() {
+    return (
+        <React.Fragment>
+        <NavbarHome/>
+            <div>
+                
+            </div>
+        <Footer/>
+        </React.Fragment>
+    );
+}
+export default HubungiKami;
